836e0438978261c4cd439d6079454f0b95ec2d85
Peter Shitote <51503440+peetlucas@users.noreply.github.com>
2023-06-13 14:10:18 +0300

    fixed postcards management route in web.php routes file (#16)

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\PostcardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostcardController;
 
 /*
 |--------------------------------------------------------------------------
