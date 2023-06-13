e5961da68feb789c6d5c82d054d8b96cc5e07ade
Peter Shitote <51503440+peetlucas@users.noreply.github.com>
2023-06-13 13:50:50 +0300

    Fix Update manage.blade.php

--- resources/views/postcards/manage.blade.php
+++ resources/views/postcards/manage.blade.php
@@ -16,35 +16,30 @@
         </tbody>
       </table>
     </header>
-    <form action="/postcards/manage">
-      <div class="relative border-2 border-gray-100 m-4 rounded-lg">
-        <div class="absolute top-4 left-3">
-          <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
-        </div>
-        <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-          placeholder="Search postcards..." />
-        <div class="absolute top-2 right-2">
-          <button type="submit" class="h-10 w-20 text-white rounded-lg bg-pink-800 hover:bg-pink-600">
-            Search
-          </button>
-        </div>
-      </div>
-    </form>
+    <!-- Search bar -->
+    @include('partials._search')
     <table class="w-full table-auto rounded-sm">
       <tbody>
         
         @unless($postcards->isEmpty())
         @foreach($postcards as $postcard)
-        <tr class="border-gray-300">
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-            <a href="/postcards/{{$postcard->id}}"> {{$postcard->title}} </a>
+        <tr class="border-gray-300">          
+          <td class="px-4 py-4 border-t border-b border-gray-300 text-lg">
+            <div class="flex">
+              <div><img class="hidden w-48 mr-6 md:block"
+              src="{{$postcard->photo ? asset('storage/' . $postcard->photo) : 'https://picsum.photos/400/200?random=' . $postcard->id }}" alt="" />
+              </div>
+              <div>
+              <a href="/postcards/{{$postcard->id}}"> {{$postcard->title}} </a>
+            </div>
+          </div>
           </td>
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-4 border-t border-b border-gray-300 text-lg">
             <a href="/postcards/{{$postcard->id}}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
                 class="fa-solid fa-pen-to-square"></i>
               Edit</a>
           </td>
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-4 border-t border-b border-gray-300 text-lg">
             <form method="POST" action="/postcards/{{$postcard->id}}">
               @csrf
               @method('DELETE')
