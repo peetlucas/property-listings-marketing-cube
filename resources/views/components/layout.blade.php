8f02ccf9999fdbd695b99da134bc5a7cfe56ec1c
Peter Shitote <51503440+peetlucas@users.noreply.github.com>
2023-06-15 13:56:17 +0300

    Fix-footer layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -70,8 +70,7 @@
     {{$slot}}
   </main>
   <footer
-
-    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-gray-600 text-white h-24 mt-24 opacity-90 md:justify-center">
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-gray-600 text-white h-8 mt-24 opacity-90 md:justify-center">
     <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>    
   </footer>
 
